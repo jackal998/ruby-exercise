119dc86febe910eb629343960e940d62a2cbbd57
jackal998 <jackal998@gmail.com>
2016-11-02 22:23:00 +0800

    kaminari

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -1,38 +1,53 @@
 class EventsController < ApplicationController
+
+before_action :set_event, :only => [:show, :edit, :destroy,:update]
+
 	def index
-		@events = Event.all
+		@events = Event.page(params[:page]).per(10)
 	end
 	def new
 		@event = Event.new
 	end
 	def create
 	  @event = Event.new(event_params)
-	  @event.save
 
-	  redirect_to :action => :index
+	  if @event.save
+	  	flash[:notice] = "新增成功"
+	  	redirect_to :action => :index
+	  else
+	  	render :action => :new
+	  end
+
+	  
 	end
 	def show
-		@event = Event.find(params[:id])
+		@page_title = @event.name
 	end
 	def edit
-		@event = Event.find(params[:id])
 	end
 	def destroy
-		@event = Event.find(params[:id])
 		@event.destroy
-		
+		flash[:alert] = "刪除成功"
 	  redirect_to :action => :index
 	end
 	def update
-		@event = Event.find(params[:id])
-		@event.update(event_params)
-
-		redirect_to :action => :show,:id => @event
+		
+		if @event.update(event_params)
+			flash[:notice] = "編輯成功"
+			redirect_to :action => :show,:id => @event
+		else
+			render :action =>	:edit
+		end
+	
 	end
 
 
 
 	private
+
+	def set_event
+		@event = Event.find(params[:id])
+	end
 
 	def event_params
 	  params.require(:event).permit(:name, :description)
