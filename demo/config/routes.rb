c71aa896a332e8a7684dd29edd5004a565a76307
jackal998 <jackal998@gmail.com>
2016-11-02 16:52:22 +0800

    add_status_to_events

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :people
-	get '/welcome/say' => 'welcome#say'
-	root 'welcome#index'
+	get 'welcome/say' => 'welcome#say'
+	get 'welcome' => 'welcome#index'
+	root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
